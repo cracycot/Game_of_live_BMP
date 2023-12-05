5ff7a85ccd6566ea8e55eeb5f5d51546612e6abd
cracycot <cracycot@mail.ru>
2023-12-05 18:12:42 +0300

    make enter and write by command line

--- main.c
+++ main.c
@@ -57,7 +57,6 @@ void print_matrix(int * a,  int size_y, int size_x) {
         printf("\n");
     }
 }
-//
 
 #pragma pack(push, 1)
 union Metadata{
@@ -92,12 +91,12 @@ uint32_t big_to_little_endian(uint32_t num) {
     return ((num>>24)&0xFF) | ((num>>8)&0xFF00) | ((num<<8)&0xFF0000) | ((num<<24)&0xFF000000);
 }
 int * print_bmp(FILE * file ) {
+    fseek(file,  0, SEEK_SET);
     union Metadata x;
     union Frame_Header y;
 
     fread(x.bytes,sizeof(char), 14, file);
     fread(y.bytes, sizeof(char), 40, file);
-    //printf("%d %d", x.fields.offset, y.fields.number_of_bits);
 
     int size = y.fields.height_pixels * y.fields.wight_pixels * (y.fields.number_of_bits / 8);
     int row_size = (y.fields.number_of_bits / 8) * y.fields.wight_pixels;
@@ -107,21 +106,15 @@ int * print_bmp(FILE * file ) {
 
     fseek(file,  x.fields.offset, SEEK_SET);
 
-    //printf(" %d  %d %d", row_padding, size, row_size);
-    //int count = 0; 000000
-
     for (int i = 0; i < y.fields.height_pixels; i += 1) {
         for (int j = 0; j < row_size  ; j += 3) {
             char *buf = (char *) malloc(sizeof(char ) * 3);
-            //count += 3;
+
             fread(buf, 3, sizeof(char), file);
 
             bmp_file[i*(y.fields.height_pixels*3 + row_padding) + j] = buf[2];
             bmp_file[i*(y.fields.height_pixels*3 + row_padding) +j + 1] = buf[1];
             bmp_file[i*(y.fields.height_pixels*3 + row_padding) + j + 2] = buf[0];
-            //printf("%d \n%d \n %d \n ", i*(y.fields.height_pixels*3 + row_padding) + j, i*(y.fields.height_pixels*3 + row_padding) + j + 1, i*(y.fields.height_pixels*3 + row_padding) + j + 2 );
-
-            //printf("\n a %d \n", i*(y.fields.height_pixels*3 + row_padding) + j + 2); color="#ffffff" color="#ffffff" e60202 color="#ffffff"
         }
         char * buf = (char *) malloc(sizeof(char ) * row_padding);
         fread(buf, row_padding, sizeof(char ), file);
@@ -138,7 +131,6 @@ int * print_bmp(FILE * file ) {
             buf[0] = bmp_file[i*(y.fields.height_pixels*3 + row_padding) + j];
             buf[1] = bmp_file[i*(y.fields.height_pixels*3 + row_padding) + j + 1];
             buf[2] = bmp_file[i*(y.fields.height_pixels*3 + row_padding) + j + 2];
-            //printf("%02x %02x %02x ",buf[0], buf[1],buf[2]);
             if (buf[0] == -1 && buf[1] ==  -1 && buf[2] ==   -1) {
                 data[index] = 0;
             }
@@ -147,15 +139,9 @@ int * print_bmp(FILE * file ) {
                 data[index] = 1;
 
             }
-            //printf(" -- %d - %d --", data[index], index);
-
             index ++;
         }
-
-
     }
-
-
     printf("\n %02x", bmp_file[315]);
     for (int i = 0; i <size/3 ; i ++) {
         if (i % row_size ==  0) {
@@ -177,7 +163,7 @@ int * print_bmp(FILE * file ) {
 }
 
 
-void * save_bmp (FILE *file, int * data) {
+void * save_bmp (FILE *file, FILE * file_write, int * data) {
     fseek(file,  0, SEEK_SET);
 
     union Metadata x;
@@ -186,43 +172,59 @@ void * save_bmp (FILE *file, int * data) {
     fread(x.bytes,sizeof(char), 14, file);
     fread(y.bytes, sizeof(char), 40, file);
 
-    int size = y.fields.height_pixels * y.fields.wight_pixels * (y.fields.number_of_bits / 8);
     int row_size = (y.fields.number_of_bits / 8) * y.fields.wight_pixels;
     int row_padding = (4 - (row_size % 4) ) % 4;
-
-    fseek(file,  x.fields.offset, SEEK_SET);
-    //ffff00 ff0000 ff1919
-    printf("\n %d \n", row_size);
-    int m = 0;
-    int i = 0;
     int count = 0;
-    int index = 0;
+    fseek(file_write,  0, SEEK_SET);
+    count += fwrite(&(x.bytes), sizeof(char), 14, file_write);
+    count += fwrite(&(y.bytes), sizeof(char), 40, file_write);
+
     for (int i = 0 ; i < y.fields.height_pixels ; i ++) { //размерность ?
         for (int j = 0; j < row_size; j ++) {
-            char * buf;
+            char * buf = (char *) malloc(sizeof(char ));
             *buf = data[((y.fields.height_pixels - i - 1) * row_size + j) / 3] == 1 ? 0 : -1;
-            fwrite(buf, sizeof(char), 1, file);
+            fseek(file_write,  0, SEEK_CUR);
+            fwrite(buf, sizeof(char), 1, file_write);
         }
-        char * buf2 = (char * ) malloc(sizeof(char ) * row_padding);
+        char * buf2 = (char *)malloc(sizeof(char ) * row_padding);
         for (int f = 0; f < row_padding; f ++) {
             buf2[f] = 0;
         }
-        fwrite(buf2, sizeof(char ), row_padding, file);
+        fseek(file_write,  0, SEEK_CUR);
+        fwrite(buf2, sizeof(char ), row_padding, file_write);
     }
 }
-int main() {
-    // insert code here...
-    //int x = readBMP();
-    FILE* file = fopen("/Users/kirilllesniak/Desktop/Test_Image_for_lab_14/Test_for_lab_14 — копия 2 3.bmp" , "r+");// открытие файла в двоичном режиме
+int main(int argc, char  ** argv) {
+    /*printf("%s \n", argv[2]);
+    printf("%s \n", argv[4]);
+    printf ("%s \n", argv[6]);
+    printf("%s \n", argv[8]);*/
+    FILE* file = fopen(argv[2] , "r+");// открытие файла в двоичном режиме
+    FILE * file_write = fopen(argv[4], "r+");
+    int max_iter = strtol(argv[6], NULL, 10) ;
+    int freq = strtol(argv[8], NULL, 10 );
     if (file == NULL) {
-        printf("Ошибка открытия файла");
+        printf("Ошибка открытия файла 1");
         return 1;
     }
-    int *x  = (int *)malloc(sizeof(int) * 100);
+    else if (file_write == NULL) {
+        printf("Ошибка открытия файла 2");
+        return 1;
+    }
+    union Metadata z;
+    union Frame_Header y;
+    fread(z.bytes,sizeof(char), 14, file);
+    fread(y.bytes, sizeof(char), 40, file);
+    int row_size =  (y.fields.number_of_bits / 8) * y.fields.wight_pixels / 3;
+    int * x  = (int *)malloc(sizeof(int) *y.fields.height_pixels * row_size);
     x = print_bmp(file);
-    gm_live(x, 10, 10);
-    print_matrix(x, 10, 10);
-    save_bmp(file, x);
+    for (int i = 0 ; i < max_iter;  i ++) {
+        if (i % freq == 0) {
+            save_bmp(file, file_write, x);
+        }
+        gm_live(x, y.fields.height_pixels, row_size);
+    }
     fclose(file);
+    fclose(file_write);
     return 0;
 }
